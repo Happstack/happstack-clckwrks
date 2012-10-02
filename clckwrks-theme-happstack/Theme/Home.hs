9af58181f9ec84cae27a6e3232a4438ab16537ac
Jeremy Shaw <jeremy@seereason.com>
2012-10-02 21:44:10 +0000

    cth-0.1.21, hdc-0.1.18: added twitter widget to homepage

--- clckwrks-theme-happstack/Theme/Home.hs
+++ clckwrks-theme-happstack/Theme/Home.hs
@@ -18,13 +18,18 @@ page :: XMLGenT (Clck ClckURL) XML
 page =
     template "happstack.com" () $
         <%>
+         <% twitter %>
          <div id="banner-box">
           <div class="mesh"></div>
 
           <div class="img-text-bg"></div>
           <div class="img-text">The relentless, uncompromised power and beauty of Haskell in a web framework.</div>
+
           <img src=(ThemeData "seven-black.png") />
+ 
          </div>
+
+
 
          <blockquote>
           <p>The relentless, uncompromised power and beauty of Haskell in a web framework.</p>
@@ -39,3 +44,44 @@ page =
 
        </%>
 
+twitter =
+   <div id="twitter">
+    <a class="twitter-timeline" data-dnt="true" href="https://twitter.com/happstack" data-widget-id="253241954832367616">Tweets by @happstack</a>
+    <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+   </div>
+
+twitterOld :: XMLGenT (Clck ClckURL) XML
+twitterOld =
+    <div id="twitter">
+      <script type="text/javascript" src="http://widgets.twimg.com/j/2/widget.js"/>
+      <script type="text/javascript">
+       new TWTR.Widget({
+        version: 2,
+        type: 'profile',
+        rpp: 8,
+        interval: 6000,
+        width: 'auto',
+        height: 300,
+        theme: {
+          shell: {
+            background: '#fff',
+            color: '#000'
+          },
+          tweets: {
+            background: '#ffffff',
+            color: '#333333',
+            links: '#111166'
+          }
+         },
+        features: {
+          scrollbar: true,
+          loop: false,
+          live: false,
+          hashtags: true,
+          timestamp: true,
+          avatars: false,
+          behavior: 'all'
+        }
+       }).render().setUser('happstack').start();
+      </script>
+  </div>
