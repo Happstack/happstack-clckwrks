c889ce18f3bac79d7cba027fbb29c43586aa7b5d
Jeremy Shaw <jeremy@seereason.com>
2012-12-06 22:03:19 +0000

    happstack-dot-com-0.2.1: Added handlers for docs and blog

--- happstack-dot-com/Main.hs
+++ happstack-dot-com/Main.hs
@@ -1,23 +1,23 @@
-{-# LANGUAGE FlexibleContexts, OverloadedStrings, RankNTypes #-}
+{-# LANGUAGE FlexibleContexts, OverloadedStrings, PackageImports, RankNTypes #-}
 module Main where
 
-import Clckwrks.URL
-import Clckwrks.Admin.Template
-import Clckwrks.GetOpts
-import Clckwrks.Monad
-import Clckwrks.Server
-import Clckwrks.Plugin
-import Clckwrks.IrcBot.Plugin
-import Clckwrks.Media.Plugin
-import Control.Applicative ((<$>))
+import Clckwrks
+-- import Clckwrks.Admin.Template
+import Clckwrks.GetOpts          (parseArgs, clckwrksOpts)
+import Clckwrks.Server           (simpleClckwrks)
+import Clckwrks.Plugin           (clckPlugin)
+import Clckwrks.IrcBot.Plugin    (ircBotPlugin)
+import Clckwrks.Media.Plugin     (mediaPlugin)
+import Control.Applicative       ((<$>))
+import Control.Monad             (msum)
 import Control.Monad.Trans
-import qualified Data.Map as Map
-import Data.Text (Text)
-import qualified Data.Text as Text
-import Happstack.Server
-import Theme
-import Web.Plugins.Core (initPlugin, setTheme)
-import System.Environment (getArgs)
+import qualified Data.Map         as Map
+import Data.Text                  (Text, unpack)
+import qualified Data.Text        as Text
+import "clckwrks-theme-happstack" Theme
+import Web.Routes                 (showURL)
+import Web.Plugins.Core           (addHandler, initPlugin, setTheme)
+import System.Environment         (getArgs)
 
 
 ------------------------------------------------------------------------------
@@ -54,8 +54,29 @@ initHook :: Text
          -> IO (ClckState, ClckwrksConfig)
 initHook baseURI clckState cc =
     do let p = plugins clckState
+       addHandler p "docs" docHandler
+       addHandler p "blog" blogHandler
        initPlugin p baseURI clckPlugin
        initPlugin p baseURI ircBotPlugin
        initPlugin p baseURI mediaPlugin
        setTheme p (Just theme)
        return (clckState, cc)
+
+
+blogHandler :: ClckPlugins -> [Text] -> ClckT ClckURL (ServerPartT IO) Response
+blogHandler _plugins [] =
+    do blogURL <- showURL Blog
+       seeOther blogURL (toResponse ())
+blogHandler _plugins ["atom.xml"] =
+    do atomURL <- showURL AtomFeed
+       seeOther atomURL (toResponse ())
+blogHandler _plugins _ =
+    do notFound (toResponse ())
+
+
+docHandler :: ClckPlugins -> [Text] -> ClckT ClckURL (ServerPartT IO) Response
+docHandler _plugins paths =
+    localRq (\rq -> rq { rqPaths = map unpack paths }) $
+      msum [ dir "crashcourse" $ serveDirectory EnableBrowsing [] "/home/jeremy/public_html/happstack-crashcourse"
+           , serveDirectory EnableBrowsing [] "/home/jeremy/public_html/happstack/7"
+           ]
